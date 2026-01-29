b28a9c8ca3131318d92cbb9454eb46dc581b322b
aashoday <aashoday.ss@gmail.com>
2026-01-30 01:06:59 +0530

    Create a linearRegression model class.
    1. Define fit method with feature scaling
    2. Build training loop
    3. Define function for predictions on test cases

---
+++ LinearRegression/linearRegression.py
@@ -0,0 +1,41 @@
+# Import libraries
+import numpy as np
+
+# Create linearRegression model class
+class LinearRegression:
+    
+    # Initialize parameters
+    def __init__(self, lr=0.001, epochs=1000):
+        self.lr = lr
+        self.epochs = epochs
+        self.weights = None
+        self.bias = None
+
+    # Fit model to data
+    def fit(self, X, y):
+        n_samples , n_features = X.shape
+
+        # Scale features
+        self.X_mean = np.mean(X, axis=0)
+        self.X_std = np.std(X, axis=0)
+        X = (X - self.X_mean) / self.X_std
+        
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        # Training loop
+        for _ in range(self.epochs):
+            y_predicted = np.dot(X, self.weights) + self.bias
+
+            dw = (1/n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1/n_samples) * np.dot(y_predicted - y)
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    # Make prediction on test cases
+    def predict(self, X):
+        X = (X - self.X_mean) / self.X_std
+        return np.dot(X, self.weights) + self.bias
+
+
