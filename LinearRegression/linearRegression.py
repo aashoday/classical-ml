Fix error in derivative of bias.

LinearRegression/linearRegression.py
@@ -28,7 +28,7 @@ class LinearRegression:
             y_predicted = np.dot(X, self.weights) + self.bias
 
             dw = (1/n_samples) * np.dot(X.T, (y_predicted - y))
-            db = (1/n_samples) * np.dot(y_predicted - y)
+            db = (1/n_samples) * np.sum(y_predicted - y)
 
             self.weights -= self.lr * dw
             self.bias -= self.lr * db