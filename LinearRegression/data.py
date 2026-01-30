cc5c0383105bdad8968cf0057e5e9e8b6bc07876
aashoday <aashoday.ss@gmail.com>
2026-01-31 00:39:24 +0530

    Implement manual split to reduce sklearn dependancy.

--- LinearRegression/data.py
+++ LinearRegression/data.py
@@ -1,21 +1,16 @@
 # Import Libraries 
 import pandas as pd
 from sklearn.datasets import fetch_california_housing
-from sklearn.model_selection import train_test_split
 
 # Fetch data
 data = fetch_california_housing(as_frame=True)
 X = data.data
 y = data.target
 
-# Set random seed 
-RANDOM_SEED = 911
-
 # Split data in training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X,
-                                                    y,
-                                                    test_size=0.2,
-                                                    random_state=RANDOM_SEED)
+SPLIT_POINT = int(0.8 * len(X))
+X_train, y_train = X.iloc[:SPLIT_POINT], y.iloc[:SPLIT_POINT] 
+X_test, y_test = X.iloc[SPLIT_POINT:], y.iloc[SPLIT_POINT:]
 
 # Print length and shape of splits 
 print(f"Length of X_train: {len(X_train)} | Shape of X_train: {X_train.shape}")
