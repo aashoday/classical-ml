b0fc139dd6d7edc4fc26eff380d1a9c2b6856da6
aashoday <aashoday.ss@gmail.com>
2026-01-31 01:45:06 +0530

    Refator code to use maeLoss from LinearRegression.py
    1. Remove mse_loss function.
    2. Add maeLoss implementation from linearRegression.py

--- LinearRegression/testLinearRegression.py
+++ LinearRegression/testLinearRegression.py
@@ -2,14 +2,9 @@ import numpy as np
 from linearRegression import LinearRegression
 from data import X_train, y_train, X_test, y_test
 
-
-# Calculate mse loss function 
-def mse_loss(y_pred, y_true):
-    return np.mean((y_true - y_pred) ** 2)
-
 regressor = LinearRegression(lr=0.001, epochs=2000)
 regressor.fit(X=X_train, y=y_train)
 preds = regressor.predict(X_test)
 
-loss_vals = mse_loss(y_pred=preds, y_true=y_test)
+loss_vals = regressor.maeLoss(preds=preds, y=y_test)
 print(loss_vals)
